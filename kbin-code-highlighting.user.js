8c44143419bdbdde55bd74c13563abde822570a5
Brad Hughes <Oricul@users.noreply.github.com>
2023-06-21 09:14:09 -0500

    Update kbin-code-highlighting.user.js
    Removed excess CSS and chose a theme that looked appealing - Base16 Windows 10.

--- kbin-code-highlighting.user.js
+++ kbin-code-highlighting.user.js
@@ -7,10 +7,8 @@
 // @license      MIT
 // @match        https://kbin.social/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kbin.social
-// @resource     css1   https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/styles/default.min.css
-// @resource     css2   https://raw.githubusercontent.com/dracula/highlightjs/master/dracula.css
-// @resource     css3   https://jmblog.github.io/color-themes-for-highlightjs/css/themes/hemisu-dark.css
-// @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/highlight.min.js
+// @resource     css   https://github.com/highlightjs/highlight.js/raw/main/src/styles/base16/windows-10.css
+// @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @downloadURL  https://github.com/Oricul/kbin-scripts/raw/main/kbin-code-highlighting.user.js
@@ -19,9 +17,7 @@
 
 (function() {
     'use strict';
-    GM_addStyle(GM_getResourceText("css1"));
-    GM_addStyle(GM_getResourceText("css2"));
-    GM_addStyle(GM_getResourceText("css3"));
+    GM_addStyle(GM_getResourceText("css"));
     function addTags(item) {
         const orig_html = item.innerHTML;
         let new_html = "<pre>" + orig_html + "</pre>";
